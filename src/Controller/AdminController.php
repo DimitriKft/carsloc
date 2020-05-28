591e8a1e20067d4bc51db8e64fdc3c89d9db7d18
klopfstein <dimitri.kft@gmail.com>
2020-05-28 14:18:03 +0200

    set up delete and finish crud basic

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Voiture;
 use App\Entity\RechercheVoiture;
 use App\Form\RechercheVoitureType;
+use App\Form\VoitureType;
 use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,4 +35,45 @@ class AdminController extends AbstractController
             "admin"    => true 
         ]);
     }
+
+    /**
+     * @Route("/admin/creation", name="creationVoiture")
+     * @Route("/admin/{id}", name="modifVoiture", methods="GET|POST")
+     */
+    public function modification(Voiture $voiture = null, Request $request, EntityManagerInterface $entitymanager)
+    {
+        if(!$voiture)
+        {
+            $voiture = new Voiture();
+        }
+        $form = $this->createForm(VoitureType::class, $voiture);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entitymanager->persist($voiture);
+            $entitymanager->flush();
+            $this->addFlash('success', "L'action a été effectué");
+            return $this->redirectToRoute("admin");
+        }
+        return $this->render('admin/modification.html.twig', [
+            "voiture" => $voiture,
+            "form"    => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("admin/{id}", name="supVoiture", methods="SUP")
+     */
+    public function suppression(Voiture $voiture, Request $request, EntityManagerInterface $entitymanager)
+    {
+        if($this->isCsrfTokenValid("SUP".$voiture->getId(), $request->get("_token")))
+        {
+            $entitymanager->remove($voiture);
+            $entitymanager->flush();
+            $this->addFlash('success', "La voiture à bien été supprimé");
+            return $this->redirectToRoute("admin");
+        }
+    }
+
 }
