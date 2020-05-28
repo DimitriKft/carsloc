05ec7ca7fac2dd9ab099aa0795a82b49cb13d730
klopfstein <dimitri.kft@gmail.com>
2020-05-28 16:41:43 +0200

    add entity utilisisateur and set up crypt password

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class GlobalController extends AbstractController
 {
@@ -13,5 +18,28 @@ class GlobalController extends AbstractController
     public function index()
     {
         return $this->render('global/acceuil.html.twig');
+    }
+
+     /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $entitymanager, UserPasswordEncoderInterface $encoder)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $passwordCrypte = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $utilisateur->setRoles("ROLE_USER");
+            $entitymanager->persist($utilisateur);
+            $entitymanager->flush();
+            $this->addFlash('success', "Vous étes correctement enregistré");
+            return $this->redirectToRoute('acceuil');
+        }
+        return $this->render('global/inscription.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
